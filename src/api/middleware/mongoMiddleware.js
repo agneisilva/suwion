f030b5c945e506f4a20afbe26ef5b85601900d73
Agnei Carvalho Silva <agnei.silva@confitec.com.br>
2021-01-12 21:16:31 -0300

    alterando mongo connection string

--- src/api/middleware/mongoMiddleware.js
+++ src/api/middleware/mongoMiddleware.js
@@ -1,6 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 
-const uri = process.env.SUWION_MONGO_CONN_STR || "mongodb://root:MongoDB2019!@localhost:27017/?authSource=admin";
+const uri = process.env.SUWION_MONGO_CONN_STR || "mongodb://root:MongoDB789!@localhost:27017/?authSource=admin";
 
 module.exports = property => {
     var connection;
