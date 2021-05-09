0133c85be39f008a019d28f66f6c1b95e579f621
Naranjo <felipe.naranjo123@gmail.com>
2021-05-09 01:21:16 -0300

    +Adicionando controle de preparo com os passos para a receita +Estruturando o componente de cadastro de receita com as functions e logando suas chamadas no console para validar o fluxo

--- src/portal/components/receita.js
+++ src/portal/components/receita.js
@@ -16,21 +16,27 @@ class CadastroReceita extends React.Component {
         super(props);
 
         this.cadastrar = this.cadastrar.bind(this);
+        this.adicionarPasso = this.adicionarPasso.bind(this);
+        this.adicionarIngrediente = this.adicionarIngrediente.bind(this);
+        this.buscarAutoCompleteIngrediente = this.buscarAutoCompleteIngrediente.bind(this);
 
         this.state = {
-            nome: "",
-            preparo: [],
-            ingredientes: [],
+            receita: {
+                nome: "",
+                preparo: [],
+                ingredientes: [],
+                rendimento: 0,
+                tempo: 0,
+                dificuldade: 0,
+            },
             ingredientesBusca: [],
-            rendimento: 0,
-            tempo: 0,
-            dificuldade: 0,
             ingredienteSelecionando: {
                 descricao: "",
                 quantia: 0,
                 undMedida: "",
                 ingrediente: null
-            }
+            },
+            passo: ""
         };
     }
 
@@ -38,6 +44,18 @@ class CadastroReceita extends React.Component {
         var valor = e.target.value;
         if (valor.length > total) {
             debugger;
+            valor = valor.substring(0, total);
+        }
+        this.setState(prevState => {
+            var altered = Object.assign({}, prevState);
+            altered.receita[campo] = valor;
+            return altered;
+        });
+    }
+
+    atribuirCarregamentoPasso(campo, total, e) {
+        var valor = e.target.value;
+        if (valor.length > total) {
             valor = valor.substring(0, total);
         }
         this.setState(prevState => {
@@ -54,19 +72,53 @@ class CadastroReceita extends React.Component {
             altered.ingredienteSelecionando[campo] = valor;
             return altered;
         });
+
+        //TODO implementar controle de delay para só buscar o auto complete depois do usuário parar de digitar por 1.5seg
+        if(campo == "descricao") this.buscarAutoCompleteIngrediente(valor);
     }
 
     cadastrar() {
+        console.log("cadastrar");
+    }
+
+    adicionarPasso() {
+        if (!!this.state.passo && this.state.passo.length > 0) {
+
+            this.setState(prevState => {
+                var altered = Object.assign({}, prevState);
+                altered.receita.preparo.push(altered.passo);
+                altered.passo = "";
+                return altered;
+            });
+        }
+    }
+
+    adicionarIngrediente() {
+        console.log("adicionarIngrediente");
+    }
+
+    buscarAutoCompleteIngrediente(descricao){
+        console.log("buscarAutoCompleteIngrediente " + descricao);
 
     }
 
     render() {
-        let { ingredientes } = this.state;
+        let { ingredientes, preparo } = this.state.receita;
         let displayIngredientes = (ingredientes.length > 0) ? <Col md={12}>
             <ListGroup>
-                {ingredientes.map((ingrediente) => {
-                    return <ListGroup.Item>
+                {ingredientes.map((ingrediente, idx) => {
+                    return <ListGroup.Item key={"ingrediente" + idx}>
                         <strong>{ingrediente.quantia} {ingrediente.undMedida}</strong> {ingrediente.descricao}
+                    </ListGroup.Item>
+                })}
+            </ListGroup>
+        </Col> : null;
+
+        let displayPreparo = (preparo.length > 0) ? <Col md={12}>
+            <ListGroup>
+                {preparo.map((passo, idx) => {
+                    return <ListGroup.Item key={"passo" + idx}>
+                        {passo}
                     </ListGroup.Item>
                 })}
             </ListGroup>
@@ -78,19 +130,19 @@ class CadastroReceita extends React.Component {
                 <Col md={2} className="text-right">Nome: </Col>
                 <Col md={10}>
                     <input id="nome"
-                        value={this.state.nome}
+                        value={this.state.receita.nome}
                         onChange={this.atribuirCarregamento.bind(this, "nome", 150)}
                         className="w-100"
                     ></input>
                     <br />
-                    <label>{this.state.nome.length}/150</label>
+                    <label>{this.state.receita.nome.length}/150</label>
                 </Col>
             </Row>
             <Row>
                 <Col md={2} className="text-right">Rendimento em porções: </Col>
                 <Col md={2}>
                     <input id="rendimento"
-                        value={this.state.rendimento}
+                        value={this.state.receita.rendimento}
                         onChange={this.atribuirCarregamento.bind(this, "rendimento", 3)}
                         className="w-100"
                     ></input>
@@ -98,7 +150,7 @@ class CadastroReceita extends React.Component {
                 <Col md={2} className="text-right">Tempo: </Col>
                 <Col md={2}>
                     <input id="tempo"
-                        value={this.state.tempo}
+                        value={this.state.receita.tempo}
                         onChange={this.atribuirCarregamento.bind(this, "tempo", 5)}
                         className="w-100"
                     ></input>
@@ -106,7 +158,7 @@ class CadastroReceita extends React.Component {
                 <Col md={2} className="text-right">Dificuldade (0 - 100): </Col>
                 <Col md={2}>
                     <input id="dificuldade"
-                        value={this.state.dificuldade}
+                        value={this.state.receita.dificuldade}
                         onChange={this.atribuirCarregamento.bind(this, "dificuldade", 3)}
                         className="w-100"
                     ></input>
@@ -133,19 +185,23 @@ class CadastroReceita extends React.Component {
                     onChange={this.atribuirCarregamentoIngrediente.bind(this, "undMedida")}
                     className="w-100"
                 ></input></Col>
-                <Col md={2}><Button>Adicionar</Button></Col>
+                <Col md={2}><Button onClick={this.adicionarIngrediente}>Adicionar</Button></Col>
                 {displayIngredientes}
             </Row>
-            {/* <Row>
+            <Row>
                 <Col md={2} className="text-right">Preparo: </Col>
-                <Col md={10}>
+                <Col md={8}>
                     <input id="preparo"
-                        value={this.state.preparo}
-                        onChange={this.atribuirCarregamento.bind(this, "preparo")}
+                        value={this.state.passo}
+                        onChange={this.atribuirCarregamentoPasso.bind(this, "passo", 500)}
                         className="w-100"
                     ></input>
+                    <br />
+                    <label>{this.state.passo.length}/500</label>
                 </Col>
-            </Row> */}
+                <Col md={2}><Button onClick={this.adicionarPasso}>Adicionar Passo</Button></Col>
+                {displayPreparo}
+            </Row>
             <Row>
                 <Col md={12} className="text-right">
                     <Button id="cadastrar" onClick={this.cadastrar}>Cadastrar</Button>
